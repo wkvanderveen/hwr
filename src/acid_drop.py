d3b647ac79813a4694b7131588d61607e4d6e57b
Tomas <tomas_vdv@hotmail.com>
2019-05-27 17:29:38 +0200

    work in progress

--- src/acid_drop.py
+++ src/acid_drop.py
@@ -19,101 +19,75 @@ nice_img_name = 'P632-Fg002-R-C01-R01'#'P583-Fg006-R-C01-R01'
 bad_img_name = 'P21-Fg006-R-C01-R01'
 ground_truth_img_name = '124-Fg004'
 
-def get_straight_path(x, y, tx):
-	line = []
-	while x != tx+1:
-		line.append((x, y))
-		x += 1
-	return line
-
-def find_target(img, x, y):
-	x += 1
-	(maxx, _) = np.shape(img)
-	while x != maxx and img[x, y] != 255: #find first white pixel
-		x += 1
-
-	return (x, y)
-
-def acid_drop(img, x, y, max_len):
-	(tx, ty) = find_target(img, x, y)
-	(maxx, maxy) = np.shape(img)
+def acid_drop(img, x, y, tx, ty, max_len):
+	(maxy, maxx) = np.shape(img)
 	heap = []
-	visitedmap = np.zeros((maxx, maxy))
-	navmap = np.zeros((maxx, maxy, 2))
-	heappush(heap, (x, y, 0))
+	visitedmap = np.zeros((maxy, maxx))
+	navmap = np.zeros((maxy, maxx, 2), dtype=np.uint16)
+	heappush(heap, (0, x, y))
 	target_found = False
 	sx = x #startvalues for x and y
 	sy = y
 
-	while not target_found and len(heap) > 0:
-		x, y, dist = heappop(heap)
+	targetv = np.array((tx, ty)) #used for dist calc
+	startv = np.array((sx, sy)) #used for dist calc
 
+	while not target_found and len(heap) > 0:
+		dist, x, y = heappop(heap)
+		
+		# print("(%d, %d)" % (x, y))
 		if x == tx and y == ty:
 			target_found = True
 		elif dist == max_len:
 			print("max path length (%d) reached.." % max_len)
-			return(get_straight_path(sx, sy, tx))
+			return []
 		else:
 			#enqueue all directions
-			if x+1 < maxx and img[x+1, y] == 255 and visitedmap[x+1, y] == 0:
-				heappush(heap, (x+1, y, dist+1))
-				visitedmap[x+1, y] = 1
-				navmap[x+1, y] = [x, y]
-			if x-1 > -1 and img[x-1, y] == 255 and visitedmap[x-1, y] == 0:
-				heappush(heap, (x-1, y, dist+1))
-				visitedmap[x-1, y] = 1
-				navmap[x-1, y] = [x, y]
-			if y+1 < maxy and img[x, y+1] == 255 and visitedmap[x, y+1] == 0:
-				heappush(heap, (x, y+1, dist+1))
-				visitedmap[x, y+1] = 1
-				navmap[x, y+1] = [x, y]
-			if y-1 > -1 and img[x, y-1] == 255 and visitedmap[x, y-1] == 0:
-				heappush(heap, (x, y-1, dist+1))
-				visitedmap[x, y-1] = 1
-				navmap[x, y-1] = [x, y]
+			if x+1 < maxx and img[y, x+1] == 255 and visitedmap[y, x+1] == 0:
+				dist = np.linalg.norm(np.array((x+1, y)) - startv)
+				heappush(heap, (dist, x+1, y))
+				visitedmap[y, x+1] = 1
+				navmap[y, x+1, 0] = x
+				navmap[y, x+1, 1] = y
+			if x-1 > -1 and img[y, x-1] == 255 and visitedmap[y, x-1] == 0:
+				dist = np.linalg.norm(np.array((x-1, y)) - startv)
+				heappush(heap, (dist, x-1, y))
+				visitedmap[y,x-1] = 1
+				navmap[y, x-1, 0] = x
+				navmap[y, x-1, 1] = y
+			if y+1 < maxy and img[y+1, x] == 255 and visitedmap[y+1, x] == 0:
+				dist = np.linalg.norm(np.array((x, y+1)) - startv)
+				heappush(heap, (dist, x, y+1))
+				visitedmap[y+1, x] = 1
+				navmap[y+1, x, 0] = x
+				navmap[y+1, x, 1] = y
+
+			if y-1 > -1 and img[y-1, x] == 255 and visitedmap[y-1, x] == 0:
+				dist = np.linalg.norm(np.array((x, y-1)) - startv)
+				heappush(heap, (dist, x, y-1))
+				visitedmap[y-1, x] = 1
+				navmap[y-1, x, 0] = x
+				navmap[y-1, x, 1] = y
 
 	if target_found:
+		print("target found!")
+		# print(navmap)
+		# print(visitedmap)
 		line = []
-		while x != sx and y != sy:
+		print(np.shape(navmap))
+		while x != sx or y != sy:
+			# print("adding to line", x, y)
 			line.append((x, y))
-			(x, y) = navmap[x, y]
+			oldx = x
+			oldy = y
+			x = navmap[oldy, oldx, 0]
+			y = navmap[oldy, oldx, 1]
 		line.append((sx, sy))
-		return list(reversed(line))
+		# print("returning line")
+		return line#np.array(reversed(line))
 
 	print("could not construct a path..")
-	return get_straight_path(sx, sy, tx)
-
-
-
-
-	
-
-def find_path(img, m, max_len = 200):
-	line = []
-
-	(maxx, _) = np.shape(img)
-	print(np.shape(img))
-
-	line.append((0, m))
-	x = 1
-	y = m
-	while x < (maxx - 2): #search until the end of the image is reached
-		print("x: ", x)
-		print("y: ", y)
-		running = True
-
-		while (x < (maxx - 2) and running):
-			running = False
-			if img[x, y] != 0: # path is clear (white) and end hasn't been reached
-				line.append((x, y))
-				x += 1
-				running = True #horrible workdarpund for no lazy eval
-
-		#apply dijkstra based acid drop
-		[line.append(item) for item in acid_drop(img, x, y, max_len)]
-		(x, y) = line[-1]
-
-	return line
+	return []
 
 
 
@@ -137,13 +111,17 @@ if __name__ == '__main__':
 	minima = l.get_minima(hist)
 
 	## actually apply acid drop
+	(_, maxx) = np.shape(img)
 
 	for m in minima:
 		print("working on minima.. %d" % (m))
-		line = find_path(img, m)
-		print("path found")
+		# line = find_path(img, m)
+		line = acid_drop(img, 0, m, maxx-1, m, 9000)
 		for (x, y) in line:
 			print("(%d,%d)" % (x, y))
 		line = np.array(line)
-		pts = line.reshape((-1,1,2))
-		img = cv2.polylines(img,[pts],True,(125))
+		# pts = line.reshape((-1,1,2))
+		img = cv2.polylines(img,[line],False,(125))
+
+
+	cv2.imwrite('a-star.png', img)
