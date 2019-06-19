Fix normalization

src/make_tfrecords.py
@@ -41,8 +41,13 @@ class TfRecordMaker(object):
 
         with tf.python_io.TFRecordWriter(tfrecord_file) as record_writer:
             for i in range(images_num):
-                image_orig = cv2.imread(image_paths[i], cv2.IMREAD_GRAYSCALE) # << 1 CHANNEL
-                normalizedImg = cv2.normalize(image_orig, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+                image_orig = cv2.imread(image_paths[i], cv2.IMREAD_GRAYSCALE)
+                normalizedImg = cv2.normalize(image_orig,
+                                              None,
+                                              alpha=0,
+                                              beta=1,
+                                              norm_type=cv2.NORM_MINMAX,
+                                              dtype=cv2.CV_32F)
                 image = cv2.imencode('.png', normalizedImg)[1].tostring()
                 boxes = dataset[image_paths[i]]
                 boxes = boxes.tostring()

src/quick_test.py
@@ -86,8 +86,6 @@ class Tester(object):
             boxes = boxes[mask]
             scores = scores[mask]
 
-
-
             if self.filters:
                 # Harder filters
                 print(f"Test: Boxes before filtering:\t{boxes.shape[0]}")
@@ -120,7 +118,7 @@ class Tester(object):
                 iou_thresh=self.iou_threshold,
                 max_boxes=self.max_boxes)
 
-            img = np.uint8(img*255)
+            # img = np.uint8(img*255)
 
             (image, results) = utils.draw_boxes(
                 img,