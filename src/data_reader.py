d69ad874d11065b3ea35ce2447634414f7ff8c99
emilettele <emilettele@hotmail.com>
2019-06-19 11:49:08 +0200

    Saving max_dimensions

--- src/data_reader.py
+++ src/data_reader.py
@@ -1,22 +1,28 @@
+import cv2
 import numpy as np
+from PIL import Image
 from skimage import io
 import os
 import sys
+
 np.set_printoptions(threshold=sys.maxsize)
 TRAIN_X_PATH = '../../data/letters-train'
 TEST_X_PATH = '../../data/letters-test'
+
 
 # Reads train and test data and converts data to numpy array
 class DataReader:
     def __init__(self):
         self.data = []
-        self.path_train = TRAIN_X_PATH #'../../data/letters/' #define path of example letter images
+        self.max_width = self.max_height = 0
+        self.path_train = TRAIN_X_PATH  # '../../data/letters/' #define path of example letter images
         self.path_test = TEST_X_PATH
         self.save_path = '../../data/'
         self.save_file_train = self.save_path + "train_letters"
         self.save_file_train_labes = self.save_path + "train_labels"
         self.save_file_test = self.save_path + "test_letters"
         self.save_file_test_labes = self.save_path + "test_labels"
+        self.save_file_dimensions = self.save_path + "max_dimensions"
         self.threshold = 200
 
     def read_letters(self, mode):
@@ -26,11 +32,16 @@ class DataReader:
             print("Reading train letters from dataset")
             num_classes = len(os.listdir(self.path_train))
             for class_num, filepath in enumerate(os.listdir(self.path_train)):
-                sub_path = self.path_train+'/'+filepath
+                sub_path = self.path_train + '/' + filepath
                 print('current letter: ', filepath)
                 for sub_file in os.listdir(sub_path):
-                    img_path = sub_path+'/'+sub_file
-                    #print(img_path)
+                    img_path = sub_path + '/' + sub_file
+                    with Image.open(img_path) as img:
+                        width, height = img.size
+                        if width > self.max_width:
+                            self.max_width = width
+                        if height > self.max_height:
+                            self.max_height = height
                     onehot = np.zeros(num_classes)
                     onehot[class_num] = 1
                     letters.append(io.imread(img_path, as_gray=True))
@@ -42,15 +53,17 @@ class DataReader:
             print("Letters saved as npy file in: ", self.save_file_train)
             np.save(self.save_file_train_labes, classes)
             print("Classes saved as npy file in: ", self.save_file_train_labes)
+            with open(self.save_file_dimensions, "w") as filename:
+                print(f"{self.max_height} {self.max_width}", file=filename)
         if mode == 'test':
             print("Reading test letters from dataset")
             num_classes = len(os.listdir(self.path_train))
             for class_num, filepath in enumerate(os.listdir(self.path_test)):
-                sub_path = self.path_test+'/'+filepath
+                sub_path = self.path_test + '/' + filepath
                 print('current letter: ', filepath)
                 for sub_file in os.listdir(sub_path):
-                    img_path = sub_path+'/'+sub_file
-                    #print(img_path)
+                    img_path = sub_path + '/' + sub_file
+                    # print(img_path)
                     onehot = np.zeros(num_classes)
                     onehot[class_num] = 1
                     letters.append(io.imread(img_path, as_gray=True))
@@ -63,8 +76,9 @@ class DataReader:
             np.save(self.save_file_test_labes, classes)
             print("Classes saved as npy file in: ", self.save_file_test_labes)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     reader = DataReader()
-    #Select mode to read train or test data
+    # Select mode to read train or test data
     reader.read_letters('train')
     reader.read_letters('test')
