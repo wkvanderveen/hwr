b48d30aadaf6c36706d32da634a521a3e1532291
Franko9999 <47304445+Franko9999@users.noreply.github.com>
2019-06-20 12:29:57 +0200

    added write to file

---
+++ src/write_to_file.py
@@ -0,0 +1,13 @@
+import os
+
+
+def write_to_file(posterior_word, filepath, filename):
+	length=len(posterior_word)
+	with open(filepath+filename+".txt", "a+", encoding="utf-8") as f:
+		for i in range(length):
+			f.write(str(posterior_word[i]))
+		f.write("\n")
+		f.close()
+	
+
+
